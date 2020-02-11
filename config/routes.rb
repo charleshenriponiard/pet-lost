3702bd745a14c55595eb3885b274a78bf051bbc9
charles-henri poniard <c.poniard@gmail.com>
2020-02-11 18:04:56 +0100

    pet routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'pets', to: 'pets#index'
+  # get 'pets/new', to: 'pets#new'
+  # post 'pets', to: 'pets#create'
+  # get 'pets/:id/edit', to: 'pets#edit'
+  # patch 'pets/:id', to: 'pets#update'
+  # get 'pets/:id', to: 'pets#show'
+  # delete 'pets/:id', to: 'pets#destroy'
 
+
+  resources :pets
 end
